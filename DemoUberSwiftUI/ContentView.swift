697404da28b4ca3f974dcae2c9728a5c3020640e
Danny-Vu <vudinhbao.nz@gmail.com>
2020-07-13 09:41:22 +0700

    Initial Commit

---
+++ DemoUberSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DemoUberSwiftUI
+//
+//  Created by Bao Vu on 7/13/20.
+//  Copyright © 2020 Bao Vu. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
